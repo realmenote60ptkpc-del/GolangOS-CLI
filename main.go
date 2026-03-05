fd21b9a9ede7188cbc10ca4bcfb5d0ed3fdca101
realmenote60ptkpc-del <realmenote60ptkpc@gmail.com>
2026-03-05 15:10:08 +0300

    Fix and update version

--- main.go
+++ main.go
@@ -28,14 +28,15 @@ func main() {
 			fmt.Println("Command list: help, exit, about.")
 
 		case "about":
-			fmt.Println("################")
-			fmt.Println("# GolangOS CLI #")
-			fmt.Println("# Version: 0.2 #")
-			fmt.Println("# Stage: Alpha #")
-			fmt.Println("################")
-			
-        case "licence":
+			fmt.Println("##################")
+			fmt.Println("# GolangOS CLI   #")
+			fmt.Println("# Version: 0.2.1 #")
+			fmt.Println("# Stage: Alpha   #")
+			fmt.Println("##################")
+
+		case "licence", "license":
 			fmt.Println("# Licensed under the Fhund Free License")
+            fmt.Println("Do whatever you want, just credit the author.")
 
 		case "exit":
 			fmt.Println("logout")
